44c4f2de408923985d3fb45ed444b4cd80963238
richardliang19 <73106388+richardliang19@users.noreply.github.com>
2022-01-29 14:32:18 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Careful</title>
+</head>
+<body alink="#007000"  bgcolor="#000000" link="gold" text="#008000" vlink="#00c000">
+<center>
+<br><br>
+<center>
+<h1>Where is the flag?</h1>
+</center>
+<br>
+<br>
+<br>
+<!--flag:{This_is_s0_simpl3}-->
+</html>
